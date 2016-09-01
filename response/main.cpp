ca06b7e60aaa1780076b209078c951b6b23593d2
João Leite <carvalhojldc@gmail.com>
2016-09-01 09:15:05 -0300

    .

--- response/main.cpp
+++ response/main.cpp
@@ -22,75 +22,10 @@ const int STD = GPIO_23;
 const int STE = GPIO_47;
 const int STF = GPIO_27;						
 const int STG =	GPIO_22;
+const int PTO = GPIO_60;
 
-void full7s() {
-	GPIO_setStatus(STA,UP);
-	GPIO_setStatus(STB,UP);
-	GPIO_setStatus(STC,UP);
-	GPIO_setStatus(STD,UP);			
-	GPIO_setStatus(STE,UP);
-	GPIO_setStatus(STF,UP);
-	GPIO_setStatus(STG,UP);
-}
-
-void clear7s() {
-	GPIO_setStatus(STA,DOWN);
-	GPIO_setStatus(STB,DOWN);
-	GPIO_setStatus(STC,DOWN);
-	GPIO_setStatus(STD,DOWN);			
-	GPIO_setStatus(STE,DOWN);
-	GPIO_setStatus(STF,DOWN);
-	GPIO_setStatus(STG,DOWN);
-}
-
-int main() {
-
-
-
-    int tempoRand = 0;
-
-    typedef std::chrono::high_resolution_clock Clock;
-    typedef std::chrono::milliseconds milliseconds;
-
-    srand((unsigned) time(NULL));
-
-    GPIO_setMode(LED,OUTPUT);
-    GPIO_setMode(BOTAO_ACAO,INPUT);
-    
-    GPIO_setMode(STA,OUTPUT);
-    GPIO_setMode(STB,OUTPUT);
-    GPIO_setMode(STC,OUTPUT);
-    GPIO_setMode(STD,OUTPUT);
-    GPIO_setMode(STE,OUTPUT);
-    GPIO_setMode(STF,OUTPUT);
-    GPIO_setMode(STG,OUTPUT);
-    
-	cout << "Fique de prontidão..." << endl;
-    while(1) {
-    	
-        tempoRand = rand()%4 + 2;
-        sleep(tempoRand);
-
-        GPIO_setStatus(LED,UP); /* Liga o led */
-        
-        Clock::time_point t0 = Clock::now();
-
-        while(!GPIO_status(BOTAO_ACAO)); /* Esperando o botao ser pressionado */
-
-        Clock::time_point t1 = Clock::now();
-
-        GPIO_setStatus(LED,DOWN); /* Desliga o led */
-
-        milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
-        float seg = ms.count() / 1000.;
-		cout << "Reação em " << seg << " s" << endl;
-
-
-		full7s();
-
-        int m = (int)seg;
-        
-        if(m == 0) {
+void geraNumero(int m, bool ponto) {
+		if(m == 0) {
 			GPIO_setStatus(STG,DOWN);     
         } else if(m == 1) {
 			GPIO_setStatus(STA,DOWN);
@@ -122,38 +57,93 @@ int main() {
 
 		} else if(m == 9) {			 		
 			GPIO_setStatus(STE,DOWN);      				
-		} else {
-			
-			// L					
-			full7s(); 							
-			GPIO_setStatus(STA,DOWN);
-			GPIO_setStatus(STB,DOWN);
-			GPIO_setStatus(STG,DOWN);
-			GPIO_setStatus(STC,DOWN);
-			sleep(1);			
-
-			// A
-			full7s(); 										
-			GPIO_setStatus(STD,DOWN);   
-			sleep(1);
-			
-	
-			// Z							
-			full7s(); 										
-			GPIO_setStatus(STF,DOWN);      				
-			GPIO_setStatus(STC,DOWN);  
-			sleep(1);
-			
-
-			// Y
-			full7s(); 										
-			GPIO_setStatus(STA,DOWN); 
-			GPIO_setStatus(STE,DOWN); 
-			sleep(1);			
-			
 		}
 		
+		if(ponto) {
+			GPIO_setStatus(PTO,UP);    
+		} else {
+			GPIO_setStatus(PTO,DOWN); 
+		}
 		
+}
+
+void full7s() {
+	GPIO_setStatus(STA,UP);
+	GPIO_setStatus(STB,UP);
+	GPIO_setStatus(STC,UP);
+	GPIO_setStatus(STD,UP);			
+	GPIO_setStatus(STE,UP);
+	GPIO_setStatus(STF,UP);
+	GPIO_setStatus(STG,UP);
+	GPIO_setStatus(PTO,UP);  
+}
+
+void clear7s() {
+	GPIO_setStatus(STA,DOWN);
+	GPIO_setStatus(STB,DOWN);
+	GPIO_setStatus(STC,DOWN);
+	GPIO_setStatus(STD,DOWN);			
+	GPIO_setStatus(STE,DOWN);
+	GPIO_setStatus(STF,DOWN);
+	GPIO_setStatus(STG,DOWN);
+	GPIO_setStatus(PTO,DOWN);
+}
+
+int main() {
+
+
+
+    int tempoRand = 0;
+
+    typedef std::chrono::high_resolution_clock Clock;
+    typedef std::chrono::milliseconds milliseconds;
+
+    srand((unsigned) time(NULL));
+
+    GPIO_setMode(LED,OUTPUT);
+    GPIO_setMode(BOTAO_ACAO,INPUT);
+    
+    GPIO_setMode(STA,OUTPUT);
+    GPIO_setMode(STB,OUTPUT);
+    GPIO_setMode(STC,OUTPUT);
+    GPIO_setMode(STD,OUTPUT);
+    GPIO_setMode(STE,OUTPUT);
+    GPIO_setMode(STF,OUTPUT);
+    GPIO_setMode(STG,OUTPUT);
+        GPIO_setMode(PTO,OUTPUT);
+    
+	cout << "Fique de prontidão..." << endl;
+    while(1) {
+    	
+        tempoRand = rand()%4 + 2;
+        sleep(tempoRand);
+
+        GPIO_setStatus(LED,UP); /* Liga o led */
+        
+        Clock::time_point t0 = Clock::now();
+
+        while(!GPIO_status(BOTAO_ACAO)); /* Esperando o botao ser pressionado */
+
+        Clock::time_point t1 = Clock::now();
+
+        GPIO_setStatus(LED,DOWN); /* Desliga o led */
+
+        milliseconds ms = std::chrono::duration_cast<milliseconds>(t1 - t0);
+        float seg = ms.count() / 1000.;
+		cout << "Reação em " << seg << " s" << endl;
+
+
+        int m = (int)seg;
+        
+		int ms2 = ((seg - m)*10);
+        
+		full7s();        
+        geraNumero(m, true);
+        sleep(2);
+        full7s();
+        geraNumero(ms2, false);
+        sleep(2);
+        clear7s();
     }
     
     return 0;
